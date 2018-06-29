5c0fe34ea91cfff2092429b992eb63799e6ef9dc
cscinsight <cscinsight.ti@yandex.com>
2018-06-28 21:50:10 -0400

    daily

--- daily.sh
+++ daily.sh
@@ -1,5 +1,5 @@
 #python /home/test/parser.py
-cp -u /home/test/newsmarkdown/. /home/m/CSC005/_post/.
+cp -u /home/test/newsmarkdown/. /home/m/CSC005/_posts/.
 git add .
 git commit -m "daily"
 git push -u origin gh-pages
