21e96f2b7c9df5c97beb8d6fa3c72c9b65f37822
cscinsight <cscinsight.ti@yandex.com>
2018-07-20 10:14:08 +0800

    daily

---
+++ delete.py
@@ -0,0 +1,16 @@
+import shutil
+import os
+import commands
+
+file_list = os.listdir('/home/website/CSC005/_post')
+num, c_date = commands.getstatusoutput('date -d -7day +%Y-%m-%d')
+print(c_date)
+
+for f in file_list:
+    f_date = f[0:10]
+    print(f_date)
+    if f_date == c_date:
+        print(True)
+        f_path = '/home/website/CSC005/_post/' + f
+        shutil.move(f_path, './oldnews')
+
