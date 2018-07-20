616335ce9ee4d7c8d6e0a09834cacb514b0783eb
cscinsight <cscinsight.ti@yandex.com>
2018-07-20 12:47:51 +0800

    daily

--- delete.py
+++ delete.py
@@ -2,7 +2,7 @@ import shutil
 import os
 import commands
 
-file_list = os.listdir('/home/website/CSC005/_post/')
+file_list = os.listdir('/home/website/CSC005/_posts/')
 num, c_date = commands.getstatusoutput('date -d -6day +%Y-%m-%d')
 print(c_date)
 
@@ -11,6 +11,6 @@ for f in file_list:
     print(f_date)
     if f_date == c_date:
         print(True)
-        f_path = '/home/website/CSC005/_post/' + f
+        f_path = '/home/website/CSC005/_posts/' + f
         os.remove(f_path)
 
